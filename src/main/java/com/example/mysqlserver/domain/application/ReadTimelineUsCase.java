cd0860bf861c52a61f600cb9489d446ee6c9891d
jinhoon.song <jinhoon.song@ahnlab.com>
2025-04-08 14:34:59 +0900

    Post - timeline 기능 추가

---
+++ src/main/java/com/example/mysqlserver/domain/application/ReadTimelineUsCase.java
@@ -0,0 +1,25 @@
+package com.example.mysqlserver.domain.application;
+
+import com.example.mysqlserver.domain.follow.application.FollowReadService;
+import com.example.mysqlserver.domain.follow.domain.Follow;
+import com.example.mysqlserver.domain.post.application.PostReadService;
+import com.example.mysqlserver.util.CursorRequest;
+import com.example.mysqlserver.util.PageCursor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ReadTimelineUsCase {
+
+    private final FollowReadService followReadService;
+
+    private final PostReadService postReadService;
+
+    public PageCursor<?> execute(Long memberId, CursorRequest cursorRequest) {
+        var follows = followReadService.read(memberId);
+        var memberIds = follows.stream().map(Follow::getToMemberId).toList();
+        return postReadService.getPosts(memberIds, cursorRequest);
+    }
+
+}
